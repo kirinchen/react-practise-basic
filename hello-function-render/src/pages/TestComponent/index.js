141042a0e65587f0e9151744c727bbf5adca1d86
kirinchen <kirin.chen1001@gmail.com>
2023-02-07 11:09:49 +0800

    update the hello-function-render/src/pages/TestComponent/index.js

--- hello-function-render/src/pages/TestComponent/index.js
+++ hello-function-render/src/pages/TestComponent/index.js
@@ -7,10 +7,10 @@ class TestComponent extends Component {
       <div className="App">
         测试组件
         <UserPage>
-          { (args)=><div>{args.name}</div> }
+          {(args) => <div>{args.name}</div>}
         </UserPage>
         <AppPages>
-          { (args)=><div>我是AppPages组件的子元素 <p>{ args.name }</p></div> }
+          {(args) => <div>我是AppPages组件的子元素 <p>{args.name} {args.idx}</p></div>}
         </AppPages>
       </div>
     );
@@ -19,22 +19,36 @@ class TestComponent extends Component {
 
 class UserPage extends Component {
 
-   render(){
-     return <div>
-       hello word
-       { this.props.children({ name:"ben" }) }
-     </div>
-   }
+  render() {
+    return <div>
+      hello word
+      {this.props.children({ name: "ben" })}
+    </div>
+  }
 }
 
-const AppPages = (props) =>{
-   const Component = props.children;
-   let ans= <div>
-     hello 你好呀
-     <Component name="ben" />
-   </div>;
-   
-   return ans;
+const genRow = (idx) => {
+  return <div>
+    ROW = {idx}
+  </div>
 }
+
+const AppPages = (props) => {
+  const Component = props.children;
+  //  let ans=;
+  let list = new Array([]);
+  for (let i = 0; i < 10; i++) {
+    list.push(<div>
+      hello 你好呀
+      <Component name="ben" idx={i} />
+
+    </div>);
+    list.push(genRow(i));
+  }
+
+  return list;
+}
+
+
 
 export default TestComponent;  
