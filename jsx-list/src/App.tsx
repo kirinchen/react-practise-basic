21adecebcf56064f6f649858433b49d66868409e
kirinchen <kirin.chen1001@gmail.com>
2023-02-08 13:46:18 +0800

    add list

--- jsx-list/src/App.tsx
+++ jsx-list/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { Fragment, JSXElementConstructor, ReactElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FragmentTest>
+      <FragmentSimplifyTest testTag="123test"></FragmentSimplifyTest>
+      <div>OTHER</div>
+      <TestMap></TestMap>
+    </FragmentTest>
+
   );
+}
+
+const FragmentTest = (props: any): JSX.Element => {
+  console.log(props.children[0].props.testTag);
+  return <Fragment>
+    {props.children[1]}
+    <li> dd </li>
+    <li> cc </li>
+    <li> vv </li>
+    {props.children[0]}
+    {props.children}
+  </Fragment>
+}
+
+
+
+const FragmentSimplifyTest = (props: any): JSX.Element => {
+  const vals = [
+    <li key={1}> s- </li>,
+    <li key={2}> b* </li>,
+    <li key={3}> x1 </li>,
+  ];
+  return <>
+    {vals}
+  </>
+}
+
+const posts = [
+  { id: "1", title: "tt11", content: "ccc11" },
+  { id: "2", title: "tt22", content: "ccc22" },
+  { id: "3", title: "tt33", content: "ccc33" }
+];
+
+const TestMap = (props: any): JSX.Element => {
+  return (
+    <div>
+      {
+        posts.map(item =>
+          <div key={item.id}>
+            {item.title}
+            {item.content}
+          </div>)
+      }
+    </div>
+  )
 }
 
 export default App;
