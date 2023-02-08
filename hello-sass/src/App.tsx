6a33501a93546b6aad839dc697bad9a554b1332e
kirinchen <kirin.chen1001@gmail.com>
2023-02-08 17:50:07 +0800

    add hello-sass

---
+++ hello-sass/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './style.scss'
+
+function App() {
+  return (
+    <div className="wrap">
+     測試
+     <h1>       this is h1     </h1>
+     <h2>       this is h2     </h2>
+     <h3>       this is h3     </h3>
+    </div>
+  );
+}
+
+export default App;
