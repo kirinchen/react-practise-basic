f36f42f12518562bd5419789f450b33b6a9cdc10
kirinchen <kirin.chen1001@gmail.com>
2023-02-09 11:17:28 +0800

    done hello-sass

--- hello-sass/src/App.tsx
+++ hello-sass/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './style.scss'
+import TestModule from './test.module.scss'
 
 function App() {
   return (
@@ -9,7 +10,13 @@ function App() {
      測試
      <h1>       this is h1     </h1>
      <h2>       this is h2     </h2>
-     <h3>       this is h3     </h3>
+     <h3 >       this is h3     </h3>
+     <div className={ TestModule.moduleClazz }>
+      vvv
+     </div>
+     <div className="globalDiv">
+      ggg
+     </div>
     </div>
   );
 }
